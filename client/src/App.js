eda67235ed12ad2993e7e72911d8c94f6d3a2f3e
daze_77 <daze77@gmail.com>
2021-03-28 19:28:40 -0400

    updated NAV added photos to home page

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route } from "react-router-dom"
 import { StoreProvider } from "./util/GlobalStore"
 
 import NavBar from "./components/NavBar"
+import NavBar2 from './components/NavBar2'
 import Footer from "./components/Footer"
 import AlertBar from "./components/AlertBar"
 // pages
@@ -10,14 +11,17 @@ import Tasks from "./pages/Tasks"
 import Register from "./pages/Register"
 import Login from "./pages/Login"
 import Logout from "./pages/Logout"
+import HomePage from "./pages/HomePage"
 function App() {
   return (
     <StoreProvider>
       <BrowserRouter>
         <div class="container">
-            <AlertBar />
+            {/* <AlertBar /> */}
             <NavBar />
-            <Route exact path={["/","/tasks"]} component={Tasks} />
+            <NavBar2 />
+            <Route exact path ={["/", "/index"]} component={HomePage}/>
+            <Route exact path={"/tasks"} component={Tasks} />
             <Route exact path="/register" component={Register} />
             <Route exact path="/login" component={Login} />
             <Route exact path="/logout" component={Logout} />
