2919354ab677f7e31aecefded184d70c2654cdda
daze_77 <daze77@gmail.com>
2021-03-31 15:37:52 -0400

    fixed error

--- client/src/pages/Resources.js
+++ client/src/pages/Resources.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import Card from "../components/Card";
+import Card from "../components/Card1";
 import APIshelter from "../util/APIshelter";
 
 
