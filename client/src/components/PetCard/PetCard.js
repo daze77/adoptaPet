88d412b0ee787dbd0e287812cf26d02aaddb2952
daze_77 <daze77@gmail.com>
2021-03-31 22:09:23 -0400

    added another modal for PetProfile to showcase additional pet information - need to add the code for passing data to this modal & card

--- client/src/components/PetCard/PetCard.js
+++ client/src/components/PetCard/PetCard.js
@@ -2,6 +2,7 @@ import React from "react";
 
 import PickMeBtn from './PickMeBtn'
 import PetRequisition from './PetRequisition'
+import PetProfile from './PetProfile'
 
 import Pets from '../../pets.json'
 
@@ -33,33 +34,15 @@ function PetCard() {
             </div>
         </div>
 
+        <PetRequisition />  
+
+        <PetProfile /> 
 
 
 
-         <PetRequisition />   
 
 
 
-
-
-        {/* This is the modal for the card click */}
-            <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-                <div class="modal-dialog">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                        </div>
-                            <div class="modal-body">
-                            ...
-                            </div>
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                            <button type="button" class="btn btn-primary">Save changes</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
                 
 
     </>
