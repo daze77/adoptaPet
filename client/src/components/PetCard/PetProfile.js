033537d98c0f8b5c9ba8b5df2a48c1ab85d5af74
daze_77 <daze77@gmail.com>
2021-04-04 19:04:15 -0400

    updated sizing of pet profile modal

--- client/src/components/PetCard/PetProfile.js
+++ client/src/components/PetCard/PetProfile.js
@@ -7,7 +7,7 @@ return (
     <>
     {/* This is the modal for the card click */}
     <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog modal-fullscreen">
+        <div class="modal-dialog modal-fullscreen-xl-down">
             <div class="modal-content">
                 <div class="modal-header">
                     <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
@@ -24,26 +24,6 @@ return (
                             <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
                         </div>
                     </div>
-
-
-
-
-
-
-
-
-                    {/* <div class="card bg-dark text-white">
-                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTskeYNmVMeGZdNiI6ahm2z647C65saNNF4Fw&usqp=CAU" class="card-img" alt="..."/>
-                        <div class="card-img-overlay">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                            <p class="card-text">Last updated 3 mins ago</p>
-                        </div>
-                    </div> */}
-                    ...
-                </div>
-                <div class="modal-footer">
-                    {/* <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button> */}
                 </div>
             </div>
         </div>
