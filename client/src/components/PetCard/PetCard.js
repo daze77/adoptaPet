474900f8ea1687f2f0f5602cb5882bfd367196cd
daze_77 <daze77@gmail.com>
2021-04-05 09:33:34 -0400

    working on props for card

--- client/src/components/PetCard/PetCard.js
+++ client/src/components/PetCard/PetCard.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useRef, useState} from "react";
 
 import PickMeBtn from './PickMeBtn'
 import PetRequisition from './PetRequisition'
@@ -12,18 +12,55 @@ function PetCard() {
     
     console.log(`this is Pets pulled from the JSON`, Pets)
 
- 
+    const id = useRef()
+    const image = useRef()
+    const name = useRef()
+    const buttonValue = useRef()
+
+
+    const [petID, setPetID] = useState()
+
+
+    
+    const petPicked = [{id: id.value, image: image, name: name.value}]
+    console.log(`huh`, petPicked)
+    
+    
+
+    const ChosenPet = (e) => {
+        console.log(`this is the button Vlaue`, e)
+        setPetID(e.target.value)
+        console.log(`pet id:`, petID)
+
+    
+
+        let bill = {
+         
+            id:petID,
+            name:name.current.value
+        }
+        console.log(bill)
+        let name2 = name.current.value
+        console.log(name2)
+        
+       
+
+
+
+
+    }
+
 
   return (
     <>
         <div>
             <div  class="row row-cols-1 row-cols-md-3 g-4" type="button" data-bs-toggle="modal" data-bs-target="#exampleModal" >
                 {Pets.map(petsinfo => (
-                    <div key={petsinfo.id} class="col">
+                    <div onClick={ChosenPet}  key={petsinfo.id} value={petsinfo._id}  ref={buttonValue}  class="col">
                         <div class="card h-100" id="petcardformat">
-                            <img src={petsinfo.image} class="card-img-top" alt="..."/>
+                            <img src={petsinfo.image} ref={image} class="card-img-top" alt="..."/>
                                 <div class="card-body">
-                                    <h5 class="card-title">{petsinfo.name}</h5>
+                                    <h5 class="card-title" ref={name}>{petsinfo.name} </h5>
                                     <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
                                 </div>
                                 <div class="card-footer">
@@ -32,7 +69,7 @@ function PetCard() {
                         </div>
                     </div>
                 ))}
-            </div>
+            </div >
         </div>
 
         <PetRequisition />  
--- client/src/components/PetCard/PetProfile.js
+++ client/src/components/PetCard/PetProfile.js
@@ -2,7 +2,8 @@ import React from "react";
 
 
 
-function PetProfile() {
+function PetProfile(props) {
+    console.log(`props on petProfile`, props)
 return (
     <>
     {/* This is the modal for the card click */}
@@ -10,14 +11,14 @@ return (
         <div class="modal-dialog modal-fullscreen-md-down">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                    <h5 class="modal-title" id="exampleModalLabel">{props.name}</h5>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
                     <div class="card mb-3" >
                         <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTskeYNmVMeGZdNiI6ahm2z647C65saNNF4Fw&usqp=CAU" class="card-img-top" alt="..." />
                         <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
+                            <h5 class="card-title">{props.name}</h5>
                             <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
                             <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
                         </div>
