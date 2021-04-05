154087aeaf78210f198a71bf62e39971605aa9c6
daze_77 <daze77@gmail.com>
2021-04-05 15:59:35 -0400

    adjusted the pets card and pets page to pass props

--- client/src/components/PetCard/PetCard.js
+++ client/src/components/PetCard/PetCard.js
@@ -68,28 +68,24 @@ function PetCard(props) {
   return (
     
     <>
-        <div>
+       
 
                     
-                <div key={props.id} class="col">
-                    <div class="card h-100" id="petcardformat">
-                        <img src={props.image} class="card-img-top" alt={props.name}/>
-                            <div class="card-body">
-                                <h5 class="card-title"  data-value={props.name} >{props.name} </h5>
-                                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                            </div>
-                            <div class="card-footer">
-                                <PickMeBtn />
-                            </div>
-                    </div>
+        <div key={props.id} class="col" type="button" data-bs-toggle="modal" data-bs-target="#exampleModal">
+            <div class="card h-100" id="petcardformat">
+                <img src={props.image} class="card-img-top" alt={props.name}/>
+                <div class="card-body">
+                    <h5 class="card-title"  data-value={props.name} >{props.name} </h5>
+                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
                 </div>
+                <div class="card-footer">
+                    <PickMeBtn />
+                </div>
+            </div>
         </div>
+     
 
-        <PetRequisition />  
 
-        <PetProfile 
-        
-        /> 
 
     </>
   );
--- client/src/pages/Pets.js
+++ client/src/pages/Pets.js
@@ -2,40 +2,33 @@ import React from 'react'
 import PetCard from '../components/PetCard/PetCard'
 import Pets from '../pets.json'
 
+import PetRequisition from '../components/PetCard/PetRequisition'
+import PetProfile from '../components/PetCard/PetProfile'
 
 
 
 
 function HomePage(){
 
-    
-
-
-
-
-
-
     return (
         <>
         <h1> Pets</h1>
 
-        <div  class="row row-cols-1 row-cols-md-3 g-4" type="button" data-bs-toggle="modal" data-bs-target="#exampleModal" >
-
-        {Pets.map(petsinfo =>(
-        
-        <PetCard
-            id={petsinfo.id}
-            key={petsinfo.id}
-            name={petsinfo.name}
-            image={petsinfo.image}
-        />
-
-       
-
-
-
-        ))} 
+        <div  class="row row-cols-1 row-cols-md-3 g-4"  >
+            {Pets.map(petsinfo =>(
+            
+            <PetCard
+                id={petsinfo.id}
+                key={petsinfo.id}
+                name={petsinfo.name}
+                image={petsinfo.image}
+            />
+            ))} 
         </div>
+
+        <PetRequisition />  
+
+        <PetProfile /> 
         </>
     )
 
