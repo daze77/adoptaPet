6e037e40b9c34c546e57cbe30b859607085d2381
daze_77 <daze77@gmail.com>
2021-04-06 22:29:04 -0400

    updated NavBar and redirects for Task page to Advice Page

--- client/src/components/NavBar/NavBar2.js
+++ client/src/components/NavBar/NavBar2.js
@@ -72,7 +72,7 @@ function NavBar2(){
                                 Reviews</NavLink>
                             </li>
                             <li className="nav-item">
-                                <NavLink to="/tasks" className="nav-link" activeClassName="active">Tasks</NavLink>
+                                <NavLink to="/advice" className="nav-link" activeClassName="active">Advice</NavLink>
                             </li>
                             {/* {showMenuItem && <li class="nav-item"> <NavLink to="/Login" className="nav-link" activeClassName="active">Login</NavLink></li>} */}
                             {!name && <div className={`collapse navbar-collapse ` + (showMenu ? 'show' : '')} id="navbar">
