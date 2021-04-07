7a5943ae2ce81e697655c6e86bbe762be24267e8
daze_77 <daze77@gmail.com>
2021-04-06 22:21:17 -0400

    updated address

---
+++ client/src/components/AddressCard.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+
+function AddressCard(){
+    return (
+
+        <>  
+            <span>123 Lost Puppy Lane</span><br/>
+            <span>Somewhere in GTA, ON</span><br/>
+            <span>Canada</span><br/>
+        </>
+    )
+}
+
+export default AddressCard
