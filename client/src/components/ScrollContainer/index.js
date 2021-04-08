93fc3248a587963cfe526896e61b3abe5b502a1d
daze_77 <daze77@gmail.com>
2021-04-08 09:45:08 -0400

    removed

--- client/src/components/ScrollContainer/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import "./style.css"
-
-function ScrollContainer(props) {
-    return <div className="container" id="scroll" style={props.style}>{props.children}</div>;
-  }
-  
-  export default ScrollContainer;
